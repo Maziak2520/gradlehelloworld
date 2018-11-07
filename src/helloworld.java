837114e893b7eadcfb61b17f0005f52326b1306b
Jiri Manas <jiri@mzx.cz>
2018-11-07 11:27:08 +0700

    initial commit

---
+++ src/helloworld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by jiri on 11/7/18.
+ */
+public class helloworld {
+        public static void main(String[] args) {
+            // Prints "Hello, World" in the terminal window.
+            System.out.println("Hello, World");
+        }
+    }
